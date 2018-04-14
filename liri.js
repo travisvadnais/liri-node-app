5f304884a8a05529751c40456ea628c9c7eb69e9
Travis Vadnais <travisvadnais@gmail.com>
2018-04-14 19:21:42 -0400

    add OMDB API and wildcard functionality

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 //Pull in the .env keys
 require("dotenv").config();
+var fs = require("fs");
 
 //Pull the key exports
 var keylist = require("./keys.js");
@@ -17,6 +18,9 @@ var Twit = require('twit')
 //Put the Twitter keys in a variable
 var T = new Twit(keylist.twitter);
 
+//OMDB package
+var omdb = require('omdb-client');
+
 //Capture the user's command to feed into the Switch statement
 var a = process.argv[2];
 
@@ -29,29 +33,36 @@ switch (a) {
     case 'my-tweets':
         getTweets();
         break;
-}
-
-
+    case 'movie-this':
+        getMovieData();
+        break;
+    case 'do-what-it-says':
+        pullFromTxtFile();
+        break;
+};
 
 //Function will run when Spotify is the command
 function searchSpotify() {
     //Set up a variable to capture the track the user wants to search for
     var titleTrack = "";
 
+    //Put arguments in an array
+    var inputs = process.argv;
+
     //This will be an array of album objects
     var albumList = [];
 
     //Capture the track we're looking for
-    for (var i = 3; i < process.argv.length; i++) {
-        titleTrack += process.argv[i] + " ";
-    }
+    for (var i = 3; i < inputs.length; i++) {
+        titleTrack += inputs[i] + " ";
+    };
 
     //Search Spotify using the keys (S)
     S.search({type: 'track', query: titleTrack, limit: 5}, function(err, data) {
         //Check for errors
         if (err) {
             return console.log("Error Occurred: " + err);
-        }
+        };
 
         //If no errors, add an object into the albumList array for each album containing the song, up to 5.
         for (var i = 0; i < data.tracks.items.length; i++) {
@@ -60,8 +71,8 @@ function searchSpotify() {
                 "Band Name": data.tracks.items[i].album.artists[0].name,
                 "Song Preview": data.tracks.items[i].preview_url,
                 "Album Title": data.tracks.items[i].album.name
-            }
-        }
+            };
+        };
         //Run a fx to make sure at least 1 result came back
         checkDefault(albumList, titleTrack);
 
@@ -69,7 +80,7 @@ function searchSpotify() {
         console.log("Results for: " + titleTrack);
         console.log(albumList);
     }); //End Spotify Search
-}
+};
 
 //This will check to see if any songs were returned and, if not, suggest Ace of Base as a default
 function checkDefault(arr, songName) {
@@ -80,10 +91,10 @@ function checkDefault(arr, songName) {
             "Song Title": "The Sign",
             "Song Preview": 'https://p.scdn.co/mp3-preview/5ca0168d6b58e0f993b2b741af90ecc7c9b16893?cid=b8831b3b77394f828a1e4e53ab3d61fd',
             "Album Title": 'The Sign (US Album) [Remastered]'
-        }
+        };
         arr.push(defaultSong);
-    }
-}
+    };
+};
 
 
 //Function to run the Twitter piece
@@ -108,9 +119,93 @@ function getTweets() {
                     "Tweet_Number": i + 1,
                     "Tweet": tweets[i].text,
                     "Timestamp": tweets[i].created_at
-                }
-            }
+                };
+            };
             console.log(tweetList);
+        };
+    });
+};
+
+function getMovieData() {
+
+    //Set up empty variable to capture movie name
+    var movieName = "";
+
+    //Capture all the user inputs in an array
+    var inputs = process.argv;
+
+    //Capture the movie we're looking for based on inputs
+    for (var i = 3; i < inputs.length; i++) {
+        movieName += inputs[i] + " ";
+    };
+
+    //Checks for user input AFTER the command.  If none, enter in Mr. Nobody
+    if (!inputs[3]) {
+        console.log("You didn't pick a movie, so please enjoy this one instead!");
+        movieName = "Mr. Nobody";
+    };
+    //Set up Parameters argument w/ the API Key & movie name
+    var params = {
+        "apiKey": "trilogy",
+        "title": movieName
+    };
+
+    //Run the GET request
+    omdb.get(params, function(err, movie) {
+
+        //Log the errors
+        if(err) {
+            return console.error(err);
+        };
+
+        //Message in case the movie isn't found.
+        if(!movie) {
+            return console.log('Movie not found!  Please Try Again!');
         }
-    })
-}
+
+        //If the movie is found, set up the movie data object to pull all pertinent info
+        else {
+            var movieData = {
+                "Title": movie.Title,
+                "Release Year": movie.Year,
+                "IMDB Rating": movie.imdbRating,
+                "Rotten Tomatoes Rating": movie.Ratings[1].Value,
+                "Country": movie.Country,
+                "Language": movie.Language,
+                "Plot": movie.Plot,
+                "Actors": movie.Actors
+            }
+            console.log(movieData);
+        }
+    });
+};
+
+function pullFromTxtFile() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        var randomArray = data.split(",");
+        var song = randomArray[1];
+        bsb(song);
+    });
+};
+
+function bsb(song) {
+    //This will be an array of album objects
+    var albumList = [];
+    S.search({type: 'track', query: song, limit: 5}, function(err, data) {
+        //Check for errors
+        if (err) {
+            return console.log("Error Occurred: " + err);
+        };
+
+        //If no errors, add an object into the albumList array for each album containing the song, up to 5.
+        for (var i = 0; i < data.tracks.items.length; i++) {
+            albumList[i] = {
+                "Song Choice": i + 1,
+                "Band Name": data.tracks.items[i].album.artists[0].name,
+                "Song Preview": data.tracks.items[i].preview_url,
+                "Album Title": data.tracks.items[i].album.name
+            };
+        };
+        console.log(albumList);
+    });
+};
